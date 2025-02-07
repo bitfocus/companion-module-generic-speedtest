cf76ec2122d384060a972e08abffc3d995b0d7d6
bryce <50299051+bryce-seifert@users.noreply.github.com>
2025-02-06 20:45:28 -0600

    fix: remove unneeded hack

--- build-config.cjs
+++
@@ -1,8 +0,0 @@
-module.exports = {
-	externals: [
-		{
-			// TODO: This is not good for number of files on disk, but is easy to implement
-			'@bryce-seifert/universal-speedtest': 'commonjs @bryce-seifert/universal-speedtest',
-		},
-	],
-}
